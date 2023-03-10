3af9d241375f99345929ae7e9b431e31f4013d31
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-10 19:09:10 +0100

    Added head component

--- views/src/index.tsx
+++ views/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import Head from './Head';
 import App from './App';
 import reportWebVitals from './test/reportWebVitals';
 
@@ -9,6 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
+        <HelmetProvider>
+            <Head />
+        </HelmetProvider>
         <App />
     </React.StrictMode>
 );
